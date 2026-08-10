2389424bc6115e44a5fe8f83626ca683b94a973b
Leaf Solar <admin@leafsolar.ng>
2026-08-10 10:08:21 +0000

    Add missing scroll imports

--- MainActivity.kt
+++ MainActivity.kt
@@ -23,6 +23,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import kotlinx.coroutines.Dispatchers
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
